1480f5f1645ba4be025021c5aace13a35fcd12f2
Mainaak Chakraborty <91618236+mainaak03@users.noreply.github.com>
2022-12-14 18:43:56 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1669A | Division?/154429675.cpp
@@ -0,0 +1,47 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long int li;
+typedef long long int ll;
+typedef double db;
+typedef long double ld;
+typedef float fl;
+const int MAX = 1e5+5;
+const ll MOD = 1e9+7;
+#define vi vector <int>
+#define vl vector <long int>
+#define vll vector <long long>
+#define sp " "
+#define nl "\n"
+#define fi(i, k, n) for (int i=k; i<n; i++)
+#define rfi(i, n, k) for (int i=n-1; i>=k; i--)
+#define all(v) v.begin(), v.end()
+#define vin(v) for (auto &i:v) {cin >>i;}
+#define pb(n) push_back(n)
+#define mp(a, b) make_pair(a, b)
+#define seti set <int>
+#define setll set <ll>
+#define lmx LLONG_MAX
+/*------------------------------------*/
+void solve() {
+    int x;
+    cin >>x;
+    if (x>=1900) cout <<"Division 1\n";
+    else if (x>=1600 and x<=1899) cout <<"Division 2\n";
+    else if (x>=1400 and x<=1599) cout <<"Division 3\n";
+    else cout <<"Division 4\n";
+}
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+ 
+    int tc=1;
+    cin >>tc;
+    while (tc--) {
+        solve();
+    }
+    return 0;
+}
