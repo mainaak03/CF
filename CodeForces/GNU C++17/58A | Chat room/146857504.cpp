3b71c2b1df79f3c1240c3a8c77d3e5583cb511ae
Mainaak Chakraborty <91618236+mainaak03@users.noreply.github.com>
2022-12-14 18:49:32 +0530

    Committing files

---
+++ CodeForces/GNU C++17/58A | Chat room/146857504.cpp
@@ -0,0 +1,57 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long int li;
+typedef long long int ll;
+typedef double db;
+typedef long double ld;
+typedef float fl;
+const int MAX = 1e5+5;
+const ll MOD = 1e9+7;
+#define vi vector <int>
+#define vl vector <long int>
+#define vll vector <long long>
+#define sp " "
+#define nl "\n"
+#define fi(k, n) for (int i=k; i<n; i++)
+#define rfi(n, k) for (int i=n-1; i>=k; i--)
+#define all(v) v.begin(), v.end()
+#define vin(v) for (auto &i:v) {cin >>i;}
+#define pb(n) push_back(n)
+#define mp(a, b) make_pair(a, b)
+#define mapi map <int>
+#define mapll map <ll>
+#define seti set <int>
+#define setll set <ll>
+/*------------------------------------*/
+void solve(string s) {
+    string m="hello";
+    bool f;
+    int idx=0;
+    for (int i=0; i<m.size(); i++) {
+        f=0;
+        for (int j=idx; j<s.size(); j++) {
+            if (s[j]==m[i]) {
+                idx=j+1; 
+                f=1;
+                break;
+            }
+        }
+        if (f==0) {
+            cout <<"NO" <<nl;
+            return;
+        }
+    }
+    cout <<"YES" <<nl;
+}
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    string s;
+    cin >>s;
+    solve(s);
+    return 0;
+}
