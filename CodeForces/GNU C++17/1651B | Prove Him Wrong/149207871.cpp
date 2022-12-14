2de68d2258cc1a8420fc3e9f6927d71889986ad2
Mainaak Chakraborty <91618236+mainaak03@users.noreply.github.com>
2022-12-14 18:46:42 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1651B | Prove Him Wrong/149207871.cpp
@@ -0,0 +1,57 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long int li;
+typedef long long int ll;
+typedef double db;
+typedef long double ld;
+typedef float fl;
+const int MAX = 1e5+5;
+const ll MOD = 1e9+7;
+#define vi vector <int>
+#define vl vector <long int>
+#define vll vector <long long>
+#define sp " "
+#define nl "\n"
+#define fi(i, k, n) for (int i=k; i<n; i++)
+#define rfi(i, n, k) for (int i=n-1; i>=k; i--)
+#define all(v) v.begin(), v.end()
+#define vin(v) for (auto &i:v) {cin >>i;}
+#define pb(n) push_back(n)
+#define mp(a, b) make_pair(a, b)
+#define seti set <int>
+#define setll set <ll>
+/*------------------------------------*/
+void solve() {
+    int n;
+    cin >>n;
+    const int MAX=1000000000;
+    vll v;
+    fi(i, 0, n) {
+        if (pow(3, i)>MAX) {
+            cout <<"NO\n" <<endl;
+            return;
+        }
+        else {
+            v.pb(pow(3, i));
+        }
+    }
+    cout <<"YES\n";
+    for (auto i:v) {
+        cout <<i <<sp;
+    }
+    cout <<nl;
+}
+     
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    int tc=1;
+    cin >>tc;
+    while (tc--) {
+        solve();
+    }
+    return 0;
+}
