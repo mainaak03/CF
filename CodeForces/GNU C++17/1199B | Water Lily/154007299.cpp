2f7d39e3ac673afb22fd18b9d9092646d9fc6af7
Mainaak Chakraborty <91618236+mainaak03@users.noreply.github.com>
2022-12-14 18:44:28 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1199B | Water Lily/154007299.cpp
@@ -0,0 +1,47 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long int li;
+typedef long long int ll;
+typedef double db;
+typedef long double ld;
+typedef float fl;
+const int MAX = 1e5+5;
+const ll MOD = 1e9+7;
+#define vi vector <int>
+#define vl vector <long int>
+#define vll vector <long long>
+#define sp " "
+#define nl "\n"
+#define fi(i, k, n) for (int i=k; i<n; i++)
+#define rfi(i, n, k) for (int i=n-1; i>=k; i--)
+#define all(v) v.begin(), v.end()
+#define vin(v) for (auto &i:v) {cin >>i;}
+#define pb(n) push_back(n)
+#define mp(a, b) make_pair(a, b)
+#define seti set <int>
+#define setll set <ll>
+#define lmx LLONG_MAX
+/*------------------------------------*/
+void solve() {
+    double h, l, res;
+    cin >>h >>l;
+    res=(l*l-h*h)/(2*h);
+    cout <<fixed <<setprecision(9) <<res;
+}
+// x^2+l^2=x^2+h^2+2hx
+//h^2+2hx-l^2=0
+// x = (l^2-h^2)/(2h)
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+ 
+    int tc=1;
+    //cin >>tc;
+    while (tc--) {
+        solve();
+    }
+    return 0;
+}
