c4a1a188f41fc19ab7d3eedf0d7cd62de49dfde5
Mainaak Chakraborty <91618236+mainaak03@users.noreply.github.com>
2022-12-14 18:46:23 +0530

    Committing files

---
+++ CodeForces/GNU C++17/459B | Pashmak and Flowers/149347210.cpp
@@ -0,0 +1,57 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long int li;
+typedef long long int ll;
+typedef double db;
+typedef long double ld;
+typedef float fl;
+const int MAX = 1e5+5;
+const ll MOD = 1e9+7;
+#define vi vector <int>
+#define vl vector <long int>
+#define vll vector <long long>
+#define sp " "
+#define nl "\n"
+#define fi(i, k, n) for (int i=k; i<n; i++)
+#define rfi(i, n, k) for (int i=n-1; i>=k; i--)
+#define all(v) v.begin(), v.end()
+#define vin(v) for (auto &i:v) {cin >>i;}
+#define pb(n) push_back(n)
+#define mp(a, b) make_pair(a, b)
+#define seti set <int>
+#define setll set <ll>
+#define LM LLONG_MAX
+/*------------------------------------*/
+void solve() {
+    ll n, cnt, cntmn, cntmx, mn, mx;
+    cin >>n;
+    vll d(n);
+    for (auto &i:d) {
+        cin >>i;
+    }
+    sort(all(d));
+    mx=d.back();
+    mn=d.front();
+    cntmx=count(all(d), mx);
+    cntmn=count(all(d), mn);
+    cnt=cntmx*cntmn;
+    if (mx==mn) {
+        cout <<mx-mn <<sp <<n*(n-1)/2;
+        return;
+    }
+    cout <<mx-mn <<sp <<cnt;
+}
+     
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    int tc=1;
+    //cin >>tc;
+    while (tc--) {
+        solve();
+    }
+    return 0;
+}
