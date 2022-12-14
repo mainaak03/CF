2986ba29a2aa1818159e42edc332a4d9d50ab3a4
Mainaak Chakraborty <91618236+mainaak03@users.noreply.github.com>
2022-12-14 18:44:48 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1020B | Badge/153435352.cpp
@@ -0,0 +1,56 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long int li;
+typedef long long int ll;
+typedef double db;
+typedef long double ld;
+typedef float fl;
+const int MAX = 1e5+5;
+const ll MOD = 1e9+7;
+#define vi vector <int>
+#define vl vector <long int>
+#define vll vector <long long>
+#define sp " "
+#define nl "\n"
+#define fi(i, k, n) for (int i=k; i<n; i++)
+#define rfi(i, n, k) for (int i=n-1; i>=k; i--)
+#define all(v) v.begin(), v.end()
+#define vin(v) for (auto &i:v) {cin >>i;}
+#define pb(n) push_back(n)
+#define mp(a, b) make_pair(a, b)
+#define seti set <int>
+#define setll set <ll>
+#define lmx LLONG_MAX
+/*------------------------------------*/
+void solve() {
+ int n;
+ cin >>n;
+ vi v(n+1);
+ fi(i, 1, n+1) {
+  cin >>v[i];
+ }
+ for (int i=1; i<n+1; i++) {
+  map<int,int> mp;
+  int ptr=i;
+  while (mp[ptr]!=2) {
+   mp[ptr]++;
+   ptr=v[ptr];
+  }
+  cout <<ptr <<sp;
+ }
+}
+     
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+ 
+    int tc=1;
+    //cin >>tc;
+    while (tc--) {
+        solve();
+    }
+    return 0;
+}
