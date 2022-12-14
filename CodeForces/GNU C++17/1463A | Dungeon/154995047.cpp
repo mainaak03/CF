f7eba18954d1e0a167574f146eb36611fd959934
Mainaak Chakraborty <91618236+mainaak03@users.noreply.github.com>
2022-12-14 18:43:38 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1463A | Dungeon/154995047.cpp
@@ -0,0 +1,62 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long int li;
+typedef long long int ll;
+typedef double db;
+typedef long double ld;
+typedef float fl;
+const int MAX = 1e5+5;
+const ll MOD = 1e9+7;
+#define vi vector <int>
+#define vl vector <long int>
+#define vll vector <long long>
+#define sp " "
+#define nl "\n"
+#define fi(i, k, n) for (int i=k; i<n; i++)
+#define rfi(i, n, k) for (int i=n-1; i>=k; i--)
+#define all(v) v.begin(), v.end()
+#define vin(v) for (auto &i:v) {cin >>i;}
+#define pb(n) push_back(n)
+#define mp(a, b) make_pair(a, b)
+#define seti set <int>
+#define setll set <ll>
+#define lmx LLONG_MAX
+/*------------------------------------*/
+void solve() {
+    int a, b, c;
+    cin >>a >>b >>c;
+    int sum=a+b+c;
+    if (sum%9!=0) {
+        cout <<"NO\n";
+    }
+    else {
+        if (sum/9<=min({a,b,c})) {
+            cout <<"YES\n";
+        }
+        else {
+            cout <<"NO\n";
+        }
+    }
+ 
+}
+     
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+ 
+    int tc=1;
+    cin >>tc;
+    while (tc--) {
+        solve();
+    }
+    return 0;
+}
+/*
+4 5 9 diff=15
+3 4 8
+2 3 7
+1 2 6 diff=6
+*/
