a0a2f6f0db93a3e4c4e794effaa7deba9bbc5e0e
Mainaak Chakraborty <91618236+mainaak03@users.noreply.github.com>
2022-12-14 18:37:01 +0530

    Committing files

---
+++ CodeForces/GNU C++14/149A | Business trip/183274053.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+#pragma GCC target ("avx2")
+#pragma GCC optimization ("O3")
+#pragma GCC optimization ("unroll-loops")
+using namespace std;
+typedef long long int ll;
+typedef double db;
+#define vi vector <int>
+#define vll vector <long long>
+#define p pair
+#define sp " "
+#define nl "\n"
+#define fi(i, k, n) for (int i=k; i<n; i++)
+#define rfi(i, n, k) for (int i=n-1; i>=k; i--)
+#define all(v) v.begin(), v.end()
+#define yes cout <<"YES\n"
+#define no cout <<"NO\n"
+bool isInt(db x) {return ceil(x)==floor(x);}
+template<typename T> istream& operator>>(istream& in, vector<T>& a) {for(auto &x : a) in >> x; return in;};
+template<typename T> ostream& operator<<(ostream& out, vector<T>& a) {for(auto &x : a) out << x << ' '; return out;};
+template<typename T1, typename T2> ostream& operator<<(ostream& out, const p<T1, T2>& x) {return out << x.first << ' ' << x.second;}
+template<typename T1, typename T2> istream& operator>>(istream& in, p<T1, T2>& x) {return in >> x.first >> x.second;}
+template<typename T> void Unique(T &a) {a.erase(unique(a.begin(), a.end()), a.end());}
+ 
+ 
+void solve(int id) {
+ int n;
+    cin>>n;
+    vi v(12), t(12);
+    cin>>v;
+    sort(all(v), greater<int>());
+    partial_sum(all(v), t.begin());
+    if (n==0) {
+        cout<<0;
+        return;
+    }
+    // cout<<v<<nl<<t;
+    fi(i,0,12) {
+        if (t[i]>=n) {
+            cout<<i+1;
+            return;
+        }
+    }
+    cout<<-1;
+}
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    int tc=1;
+    // cin >>tc;
+ fi(i,0,tc) {
+  solve(i+1);
+ }
+}
