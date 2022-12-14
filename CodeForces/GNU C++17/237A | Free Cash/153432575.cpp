75e7d1b51d6eacd71b88d6e2f87f2c374d1ccb56
Mainaak Chakraborty <91618236+mainaak03@users.noreply.github.com>
2022-12-14 18:44:52 +0530

    Committing files

---
+++ CodeForces/GNU C++17/237A | Free Cash/153432575.cpp
@@ -0,0 +1,56 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long int li;
+typedef long long int ll;
+typedef double db;
+typedef long double ld;
+typedef float fl;
+const int MAX = 1e5+5;
+const ll MOD = 1e9+7;
+#define vi vector <int>
+#define vl vector <long int>
+#define vll vector <long long>
+#define sp " "
+#define nl "\n"
+#define fi(i, k, n) for (int i=k; i<n; i++)
+#define rfi(i, n, k) for (int i=n-1; i>=k; i--)
+#define all(v) v.begin(), v.end()
+#define vin(v) for (auto &i:v) {cin >>i;}
+#define pb(n) push_back(n)
+#define mp(a, b) make_pair(a, b)
+#define seti set <int>
+#define setll set <ll>
+#define lmx LLONG_MAX
+/*------------------------------------*/
+void solve() {
+ int h, m, n;
+ cin >>n;
+ set<pair<int,int>> st;
+ vector<pair<int,int>> v;
+ while (n--) {
+  cin >>h >>m;
+  st.insert(mp(h, m));
+  v.pb(mp(h, m));
+ }
+ int mx=0;
+ for (auto i:st) {
+  int c=count(all(v),i);
+  if (c>mx) mx=c;
+ }
+ cout <<mx <<nl;
+}
+     
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+ 
+    int tc=1;
+    //cin >>tc;
+    while (tc--) {
+        solve();
+    }
+    return 0;
+}
