fc9ed3c5ba29ad0fd3fee471a0011f60c373fc9b
Mainaak Chakraborty <91618236+mainaak03@users.noreply.github.com>
2022-12-14 18:40:10 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1690B | Array Decrements/159760621.cpp
@@ -0,0 +1,55 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long int li;
+typedef long long int ll;
+typedef double db;
+typedef long double ld;
+typedef float fl;
+const int MAX = 1e5+5;
+const ll MOD = 1e9+7;
+#define vi vector <int>
+#define vl vector <long int>
+#define vll vector <long long>
+#define sp " "
+#define nl "\n"
+#define fi(i, k, n) for (int i=k; i<n; i++)
+#define rfi(i, n, k) for (int i=n-1; i>=k; i--)
+#define all(v) v.begin(), v.end()
+#define vin(v) for (auto &i:v) {cin >>i;}
+#define pb(n) push_back(n)
+#define mp(a, b) make_pair(a, b)
+#define seti set <int>
+#define setll set <ll>
+#define pll pair<ll,ll>
+#define lmx LLONG_MAX
+#define yes cout <<"YES\n"
+#define no cout <<"NO\n"
+/*------------------------------------*/
+void solve() {
+    int n;
+    cin >>n;
+    vi a(n), b(n);
+    vin(a); vin(b);
+    int diff=0;
+    fi(i,0,n) {
+        if (a[i]-b[i]<0) {no; return;}
+        diff=max(diff, a[i]-b[i]);
+    }
+    fi(i,0,n) {
+        if (a[i]-b[i]<diff and b[i]!=0) {no; return;}
+    }
+    yes;
+}
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    int tc=1;
+    cin >>tc;
+    while (tc--) {
+        solve();
+    }
+}
