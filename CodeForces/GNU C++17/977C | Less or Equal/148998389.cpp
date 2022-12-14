a1b5330178e8a4af7028d6695bb6e080ce10d97b
Mainaak Chakraborty <91618236+mainaak03@users.noreply.github.com>
2022-12-14 18:47:05 +0530

    Committing files

---
+++ CodeForces/GNU C++17/977C | Less or Equal/148998389.cpp
@@ -0,0 +1,66 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long int li;
+typedef long long int ll;
+typedef double db;
+typedef long double ld;
+typedef float fl;
+const int MAX = 1e5+5;
+const ll MOD = 1e9+7;
+#define vi vector <int>
+#define vl vector <long int>
+#define vll vector <long long>
+#define sp " "
+#define nl "\n"
+#define fi(i, k, n) for (int i=k; i<n; i++)
+#define rfi(i, n, k) for (int i=n-1; i>=k; i--)
+#define all(v) v.begin(), v.end()
+#define vin(v) for (auto &i:v) {cin >>i;}
+#define pb(n) push_back(n)
+#define mp(a, b) make_pair(a, b)
+#define seti set <int>
+#define setll set <ll>
+/*------------------------------------*/
+void solve() {
+    int n, k;
+    cin >>n >>k;
+    vi v(n);
+    vin(v);
+    sort(all(v));
+    int idx=0;
+    if (k==n) {
+        cout <<v.back();
+    }
+    else if (k==0) {
+        if (v[0]>1) {
+            cout <<v[0]-1;
+        }
+        else {
+            cout <<"-1";
+        }
+    }
+    else if (k<n) {
+        idx=k-1;
+        if (v[idx]==v[idx+1]) {
+            cout <<"-1";
+        }
+        else {
+            cout <<v[idx];
+        }
+    }
+    //1 3 3 5 7 10 20
+}
+     
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    int tc=1;
+    //cin >>tc;
+    while (tc--) {
+        solve();
+    }
+    return 0;
+}
