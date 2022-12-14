9d6c15aa8297f77afaae449112df2e316234cd99
Mainaak Chakraborty <91618236+mainaak03@users.noreply.github.com>
2022-12-14 18:40:57 +0530

    Committing files

---
+++ CodeForces/GNU C++17/155A | I_love_\%username\%/157419690.cpp
@@ -0,0 +1,89 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long int li;
+typedef long long int ll;
+typedef double db;
+typedef long double ld;
+typedef float fl;
+const int MAX = 1e5+5;
+const ll MOD = 1e9+7;
+#define vi vector <int>
+#define vl vector <long int>
+#define vll vector <long long>
+#define sp " "
+#define nl "\n"
+#define fi(i, k, n) for (int i=k; i<n; i++)
+#define rfi(i, n, k) for (int i=n-1; i>=k; i--)
+#define all(v) v.begin(), v.end()
+#define vin(v) for (auto &i:v) {cin >>i;}
+#define pb(n) push_back(n)
+#define mp(a, b) make_pair(a, b)
+#define seti set <int>
+#define setll set <ll>
+#define pll pair<ll,ll>
+#define lmx LLONG_MAX
+#define yes cout <<"YES\n"
+#define no cout <<"NO\n"
+/*------------------------------------*/
+vll t;
+void build(vll a, int v, int tl, int tr) {
+    if (tl == tr) {
+        t[v] = a[tl];
+    } else {
+        int tm = (tl + tr) / 2;
+        build(a, v*2, tl, tm);
+        build(a, v*2+1, tm+1, tr);
+        t[v] = t[v*2] + t[v*2+1];
+    }
+}
+int sum(int v, int tl, int tr, int l, int r) {
+    if (l > r) 
+        return 0;
+    if (l == tl && r == tr) {
+        return t[v];
+    }
+    int tm = (tl + tr) / 2;
+    return sum(v*2, tl, tm, l, min(r, tm))
+           + sum(v*2+1, tm+1, tr, max(l, tm+1), r);
+}
+void update(int v, int tl, int tr, int pos, int new_val) {
+    if (tl == tr) {
+        t[v] = new_val;
+    } else {
+        int tm = (tl + tr) / 2;
+        if (pos <= tm)
+            update(v*2, tl, tm, pos, new_val);
+        else
+            update(v*2+1, tm+1, tr, pos, new_val);
+        t[v] = t[v*2] + t[v*2+1];
+    }
+}
+void solve() {
+    int n, mn=0, mx=0, cnt=0;
+    cin >>n;
+    vi v(n);
+    vin(v);
+    mn=mx=v[0];
+    fi(i, 1, n) {
+        if (v[i]>mx) {
+            mx=v[i]; cnt++;
+        }
+        else if (v[i]<mn) {
+            mn=v[i]; cnt++;
+        }
+    }
+    cout <<cnt <<nl;
+}
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    int tc=1;
+    //cin >>tc;
+    while (tc--) {
+        solve();
+    }
+}
