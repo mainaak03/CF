89e1c9b6f821d15788a1712a041e5d868653af05
Mainaak Chakraborty <91618236+mainaak03@users.noreply.github.com>
2022-12-14 18:34:11 +0530

    Committing files

---
+++ CodeForces/GNU C++14/296A | Yaroslav and Permutations/184708685.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+#pragma GCC target ("avx2")
+#pragma GCC optimization ("O3")
+#pragma GCC optimization ("unroll-loops")
+using namespace std;
+typedef long long int ll;
+typedef double db;
+#define vi vector <int>
+#define vll vector <long long>
+#define p pair
+#define sp " "
+#define nl "\n"
+#define fi(i, k, n) for (ll i=k; i<n; i++)
+#define rfi(i, n, k) for (ll i=n-1; i>=k; i--)
+#define all(v) v.begin(), v.end()
+#define yes cout <<"YES\n"
+#define no cout <<"NO\n"
+bool isInt(db x) {return ceil(x)==floor(x);}
+template<typename T> istream& operator>>(istream& in, vector<T>& a) {for(auto &x : a) in >> x; return in;};
+template<typename T> ostream& operator<<(ostream& out, vector<T>& a) {for(auto &x : a) out << x << ' '; return out;};
+template<typename T1, typename T2> ostream& operator<<(ostream& out, const p<T1, T2>& x) {return out << x.first << ' ' << x.second;}
+template<typename T1, typename T2> istream& operator>>(istream& in, p<T1, T2>& x) {return in >> x.first >> x.second;}
+template<typename T> void Unique(T &a) {a.erase(unique(a.begin(), a.end()), a.end());}
+#define TxtIO   freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
+#define mod 1000000007
+ 
+inline bool chk(map<int,int> mp) {
+    bool ok=0;
+    for (auto i:mp) {
+        if (i.second) ok=1;
+    }
+    return ok;
+}
+ 
+void solve(int id) {
+    int n;
+    cin>>n;
+    vi v(n), t;
+    cin>>v;
+    map<int,int> mp;
+    for (auto i:v) mp[i]++;
+    for (auto i:mp) {
+        if (i.second>(n+1)/2) {
+            no;
+            return;
+        }
+    }
+    yes;
+}
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    // TxtIO
+    int tc=1;
+    // cin >>tc;
+ fi(i,0,tc) {
+  solve(i+1);
+ }
+}
