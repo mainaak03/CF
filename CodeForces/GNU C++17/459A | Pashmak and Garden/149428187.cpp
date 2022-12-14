1711c59ac291b634ba55bc26e8b00a0446605fd0
Mainaak Chakraborty <91618236+mainaak03@users.noreply.github.com>
2022-12-14 18:46:19 +0530

    Committing files

---
+++ CodeForces/GNU C++17/459A | Pashmak and Garden/149428187.cpp
@@ -0,0 +1,50 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long int li;
+typedef long long int ll;
+typedef double db;
+typedef long double ld;
+typedef float fl;
+const int MAX = 1e5+5;
+const ll MOD = 1e9+7;
+#define vi vector <int>
+#define vl vector <long int>
+#define vll vector <long long>
+#define sp " "
+#define nl "\n"
+#define fi(i, k, n) for (int i=k; i<n; i++)
+#define rfi(i, n, k) for (int i=n-1; i>=k; i--)
+#define all(v) v.begin(), v.end()
+#define vin(v) for (auto &i:v) {cin >>i;}
+#define pb(n) push_back(n)
+#define mp(a, b) make_pair(a, b)
+#define seti set <int>
+#define setll set <ll>
+#define LM LLONG_MAX
+/*------------------------------------*/
+void solve() {
+    int x1, y1, x2, y2;
+    cin >>x1 >>y1 >>x2 >>y2;
+        if (x1 != x2 and y1 != y2 and abs(x1 - x2) != abs(y1 - y2))
+        cout << -1 << endl;
+    else if (x1 == x2)
+        cout << x1 + abs(y1 - y2) << " " << y1 << " " << x2 + abs(y1 - y2) << " " << y2 << endl;
+    else if (y1 == y2)
+        cout << x1 << " " << y1 + abs(x1 - x2) << " " << x2 << " " << y2 + abs(x1 - x2) << endl;
+    else
+        cout << x1 << " " << y2 << " " << x2 << " " << y1 << endl;
+}
+     
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    int tc=1;
+    //cin >>tc;
+    while (tc--) {
+        solve();
+    }
+    return 0;
+}
