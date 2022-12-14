b20684a426c583a4c4d4679e0f1253b1fb9b9b99
Mainaak Chakraborty <91618236+mainaak03@users.noreply.github.com>
2022-12-14 18:39:33 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1703A | YES or YES?/163816438.cpp
@@ -0,0 +1,50 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long int li;
+typedef long long int ll;
+typedef double db;
+typedef long double ld;
+typedef float fl;
+const int MAX = 1e5+5;
+const ll MOD = 1e9+7;
+#define vi vector <int>
+#define vl vector <long int>
+#define vll vector <long long>
+#define sp " "
+#define nl "\n"
+#define fi(i, k, n) for (int i=k; i<n; i++)
+#define rfi(i, n, k) for (int i=n-1; i>=k; i--)
+#define all(v) v.begin(), v.end()
+#define vin(v) for (auto &i:v) {cin >>i;}
+#define pb(n) push_back(n)
+#define mp(a, b) make_pair(a, b)
+#define seti set <int>
+#define setll set <ll>
+#define useti unordered_set <int>
+#define usetll unordered_set <ll>
+#define pll pair<ll,ll>
+#define lmx LLONG_MAX
+#define yes cout <<"YES\n"
+#define no cout <<"NO\n"
+#define ss second
+#define ff first
+/*------------------------------------*/
+void solve() {
+    string s;
+    cin>>s;
+    if ((s[0]=='y' or s[0]=='Y') and (s[1]=='e' or s[1]=='E') and (s[2]=='s' or s[2]=='S')) yes;
+    else no;
+}
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    int tc=1;
+    cin >>tc;
+    while (tc--) {
+        solve();
+    }
+}
