6b3c93021b86b2e7cb7ff0d8c9db92cd5ec12107
Mainaak Chakraborty <91618236+mainaak03@users.noreply.github.com>
2022-12-14 18:48:11 +0530

    Committing files

---
+++ CodeForces/GNU C++17/450A | Jzzhu and Children/147366425.cpp
@@ -0,0 +1,60 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long int li;
+typedef long long int ll;
+typedef double db;
+typedef long double ld;
+typedef float fl;
+const int MAX = 1e5+5;
+const ll MOD = 1e9+7;
+#define vi vector <int>
+#define vl vector <long int>
+#define vll vector <long long>
+#define sp " "
+#define nl "\n"
+#define fi(k, n) for (int i=k; i<n; i++)
+#define rfi(n, k) for (int i=n-1; i>=k; i--)
+#define all(v) v.begin(), v.end()
+#define vin(v) for (auto &i:v) {cin >>i;}
+#define pb(n) push_back(n)
+#define mp(a, b) make_pair(a, b)
+#define seti set <int>
+#define setll set <ll>
+/*------------------------------------*/
+void solve() {
+    int n, m;
+    cin >>n >>m;
+    vi v(n);
+    vin(v);
+    queue<pair<int, int>> p;
+    fi(0, n) {
+        p.push(mp(v[i], i));
+    }
+    pair<int,int> t;
+    while (p.size()>1) {
+        if (m>=p.front().first) {
+            p.pop();
+        }
+        else {
+            t=p.front();
+            t.first-=m;
+            p.pop();
+            p.push(t);
+        }
+    }
+    cout <<p.front().second+1;
+}
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    //int tc;
+    //cin >>tc;
+    //while (tc--) {
+        solve();
+    //}
+    return 0;
+}
