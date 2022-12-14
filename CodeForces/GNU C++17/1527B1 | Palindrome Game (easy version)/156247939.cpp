16e35b9e7e9304680692876a20048e1387cee2ea
Mainaak Chakraborty <91618236+mainaak03@users.noreply.github.com>
2022-12-14 18:41:47 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1527B1 | Palindrome Game (easy version)/156247939.cpp
@@ -0,0 +1,54 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long int li;
+typedef long long int ll;
+typedef double db;
+typedef long double ld;
+typedef float fl;
+const int MAX = 1e5+5;
+const ll MOD = 1000000007;
+const double pi = 3.141593;
+ll mod(ll a, ll b) {return ((a%b)+b)%b;}
+#define vi vector <int>
+#define vl vector <long int>
+#define vll vector <long long>
+#define sp " "
+#define nl "\n"
+#define fi(i, k, n) for (int i=k; i<n; i++)
+#define rfi(i, n, k) for (int i=n-1; i>=k; i--)
+#define all(v) v.begin(), v.end()
+#define vin(v) for (auto &i:v) {cin >>i;}
+#define pb(n) push_back(n)
+#define mp(a, b) make_pair(a, b)
+#define seti set <int>
+#define setll set <ll>
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define lmx LLONG_MAX
+#define yes cout <<"YES\n"
+#define no cout <<"NO\n"
+/*------------------------------------*/
+void solve() {
+    int n, cnt=0;
+    string s;
+    cin >>n >>s;
+    fi(i, 0, n) {
+        if (s[i]=='0') cnt++;
+    }
+    if (cnt==1) {cout <<"BOB\n"; return;}
+    (cnt%2)?cout<<"ALICE\n":cout<<"BOB\n";
+}
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    int tc=1;
+    cin >>tc;
+    while (tc--) {
+        solve();
+    }
+}
