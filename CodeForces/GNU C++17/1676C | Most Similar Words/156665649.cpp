8c62e51f03bda6d3f0d3e4968d32e02cc7b81dfa
Mainaak Chakraborty <91618236+mainaak03@users.noreply.github.com>
2022-12-14 18:41:15 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1676C | Most Similar Words/156665649.cpp
@@ -0,0 +1,61 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long int li;
+typedef long long int ll;
+typedef double db;
+typedef long double ld;
+typedef float fl;
+const int MAX = 1e5+5;
+const ll MOD = 1e9+7;
+#define vi vector <int>
+#define vl vector <long int>
+#define vll vector <long long>
+#define sp " "
+#define nl "\n"
+#define fi(i, k, n) for (int i=k; i<n; i++)
+#define rfi(i, n, k) for (int i=n-1; i>=k; i--)
+#define all(v) v.begin(), v.end()
+#define vin(v) for (auto &i:v) {cin >>i;}
+#define pb(n) push_back(n)
+#define mp(a, b) make_pair(a, b)
+#define seti set <int>
+#define setll set <ll>
+#define pll pair<ll,ll>
+#define lmx LLONG_MAX
+#define yes cout <<"YES\n"
+#define no cout <<"NO\n"
+/*------------------------------------*/
+inline int f(string a, string b, int n) {
+    int cnt=0;
+    fi(i, 0, n) {
+        cnt+=abs(a[i]-b[i]);
+    }
+    return cnt;
+}
+ 
+void solve() {
+    ll n, m, mn=lmx;
+    cin >>n >>m;
+    vector<string> v(n);
+    vin(v);
+    fi(i, 0, n) {
+        fi(j, i+1, n) {
+            mn=min(mn,(ll)f(v[i], v[j], m));
+        }
+    }
+    cout <<mn <<nl;
+}
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    int tc=1;
+    cin >>tc;
+    while (tc--) {
+        solve();
+    }
+    return 0;
+}
